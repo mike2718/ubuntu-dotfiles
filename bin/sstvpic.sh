873701a8f3de15d677dd5928278d31e41b7a02de
Mike Akiba <mike2718@gmail.com>
2023-05-12 19:29:18 +0800

    更新

--- bin/sstvpic.sh
+++ bin/sstvpic.sh
@@ -2,6 +2,6 @@
 # sstv picture, BG7XTQ
 # shell script to encode SSTV image
 # add callsign
-mogrify -font Noto-Sans-Mono-SemiBold -pointsize 72 -draw "text 10,60 'BG7XTQ CQ SSTV'" image.png
+mogrify -font Noto-Sans-Mono-SemiBold -pointsize 72 -draw "text 10,60 'BG7XTQ CQ SSTV'" -fill red3 image.png
 pisstv -r 22050 -p pd120 image.png
 #sudo ./pifm_sstv /tmp/image.png.wav
