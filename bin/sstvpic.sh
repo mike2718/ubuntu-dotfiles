b8d38ef3edf14c2b6ae1bc2b2ff7c2545946e959
Mike Akiba <mike2718@gmail.com>
2023-06-06 21:19:28 +0800

    更新

--- bin/sstvpic.sh
+++ bin/sstvpic.sh
@@ -2,6 +2,14 @@
 # sstv picture, BG7XTQ
 # shell script to encode SSTV image
 # add callsign
-mogrify -font Noto-Sans-Mono-SemiBold -pointsize 72 -draw "text 10,60 'BG7XTQ CQ SSTV'" -fill red3 image.png
+rm -f image.png image.png.wav
+cp image0.png image.png
+mogrify -font Noto-Sans-Mono-SemiBold -pointsize 72 -draw "text 10,60 'CQ SSTV    de BG7XTQ'" -fill red3 image.png
+
+#竖向的图片(800x616)
+mogrify -font Noto-Sans-Mono-SemiBold -pointsize 72 -draw "text 10,60 'CQ SSTV' text 410,60 'de BG7XTQ'" -fill green3 image.png
 pisstv -r 22050 -p pd120 image.png
 #sudo ./pifm_sstv /tmp/image.png.wav
+
+sox -n -r 22050 -c 1 /tmp/silence.wav trim 0.0 5
+sox $(for f in *.wav; do echo -n "$f /tmp/silence.wav "; done) output.wav
